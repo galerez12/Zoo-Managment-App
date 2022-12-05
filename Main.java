0b138269a25d7c521ffdd65f464380d601066369
galerez12 <101173666+galerez12@users.noreply.github.com>
2022-12-05 18:10:48 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,12 +1,5 @@
 import java.io.*;
 
-//Gal erez - 313309213
-//Reef Rosenblat - 206134645
-//Golan Parsha - 305001885
-//Shagansh Kasta - 324488261
-
-//באישורך מההתכתבות במייל, מחלקת המיין שלנו רק יוצרת את האובייקט הראשי שבו מוחזקת ומונהלת המערכת כולה
-
 public class Main
 {
     //Main method
